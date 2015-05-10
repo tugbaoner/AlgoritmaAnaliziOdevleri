fcd818147a475407c2a17e3303da5e2674e06a2c
tugbaoner <tuugbaoner@gmail.com>
2015-05-10 22:41:45 +0300

    MaxSUBsuM2(En_guncel_hali)
    MaxSUBsuM2(En_guncel_hali)

---
+++ MaxSUBsuM2/MaxSUBsuM2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaxSUBsuM2
+{
+    class Program
+    {
+        static private int bas = 0;
+        static private int son = -1;
+
+
+        static void Main(string[] args)
+        {
+            int maxTop;
+            Console.WriteLine("Dizinizin eleman sayisini giriniz");
+            int indis = Int32.Parse(Console.ReadLine());
+            int[] dizi = new int[indis];
+            Random rasgele = new Random();
+            for (int k = 0; k < dizi.Length; k++)
+            {
+                int sayi = rasgele.Next(-50, 50);
+                dizi[k] = sayi;
+            }
+            Console.WriteLine("Dizinin elemanları");
+            for (int j = 0; j < dizi.Length; j++)
+            {
+                Console.WriteLine("dizinin " + (j + 1) + ". elemani :" + dizi[j]);
+            }
+
+            maxTop = MaxSUBsuM(dizi);
+            Console.WriteLine("maxTop = " + maxTop);
+            for (int l = bas; l <= son; l++)
+            {
+                Console.Write("dizi[" + l + "]  ");
+            }
+            Console.ReadLine();
+        }
+
+        private static int MaxSUBsuM(int[] dizi)
+        {
+            int maxTop = 0;
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                for (int j = i; j < dizi.Length; j++)
+                {
+                    int thisTop = 0;
+                    for (int k = i; k <= j; k++)
+                    {
+                        thisTop += dizi[k];
+                        
+                    }
+                    if (thisTop > maxTop)
+                    {
+                        maxTop = thisTop;
+                        bas = i;
+                        son = j;
+
+                    }
+                }
+            }
+            return maxTop;
+        }
+    }
+}
