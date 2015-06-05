a272f1ed8b49fb93ee7aa725ee3912521300a5d4
tugbaoner <tuugbaoner@gmail.com>
2015-06-05 11:30:53 +0300

    SinavSorusu_5
    SinavSorusu_5

---
+++ SinavSorusu_5/SinavSorusu_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SinavSorusu_5
+{
+    class Program
+    {
+        private static Search(Node key, Node t) { 
+            if(t==null)                    
+               return null;                 
+            else if(key<t.key)             
+                return Search(key,t.leftChild);  
+           else if(key>t.key)             
+                return Search(key,t.rightChild); 
+           else                           
+               return t;    
+        }  
+        static void Main(string[] args){
+        }
+        
+        
+    }
+}
