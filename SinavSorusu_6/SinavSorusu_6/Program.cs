f4877e18d9d6bdff7e433a068ef81e16cffffc68
tugbaoner <tuugbaoner@gmail.com>
2015-06-05 11:32:49 +0300

    SinavSorusu_6
    SinavSorusu_6

---
+++ SinavSorusu_6/SinavSorusu_6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SinavSorusu_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = { 0, 0, 0, 1, 1, 0, 1, 0, 0 ,0,0};
+            
+            int maxSifir = 0;
+            int sifir = 0;
+            
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                
+                if (dizi[i] == 0)
+                {
+                    
+                    sifir++;
+                    if (sifir > maxSifir)
+                    {
+                        maxSifir = sifir;
+                        
+                     }
+                 
+                if (dizi[i] == 1)
+                { 
+                    sifir = 0;
+
+                    }
+               }
+            }
+            Console.WriteLine(maxSifir);
+            Console.ReadLine();
+        }
+    }
+}
